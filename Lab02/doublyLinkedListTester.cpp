f4d6bf03ad3454c14f33fdf58793b7ff9297b7e0
tyleraharrison <tyleraharrison@gmail.com>
2021-01-30 19:33:23 -0600

    Upload original Lab02

---
+++ Lab02/doublyLinkedListTester.cpp
@@ -0,0 +1,15 @@
+//Program to test various operations on a doubly linked list
+  
+#include <iostream>
+#include "doublyLinkedList.h"  
+ 
+using namespace std;  
+
+int main()
+{
+	cout << "See Programming Exercise 12 at the end of the chapter" << endl;
+	doublyLinkedList<int> dlList;
+	dlList.insert(12);
+
+	return 0;
+} 
