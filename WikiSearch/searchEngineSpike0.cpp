06547d5637d233eecefab2018969a59a580983ff
tyleraharrison <tyleraharrison@gmail.com>
2021-04-15 12:20:36 -0500

    Spike 0 complete

--- WikiSearch/searchEngineSpike0.cpp
+++ WikiSearch/searchEngineSpike0.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <set>
 
 int main()
 {
@@ -9,8 +10,7 @@ int main()
   std::vector<int> v1 = {5, 10, 15, 20, 50};
   std::vector<int> v2 = {50, 40, 30, 20, 5};
   std::vector<int> v3 = {1, 2, 20, 4, 5, 50};
-  std::vector<int> v4 = {5, 20, 31, 200, 51};
-  std::vector<int> vResult;
+  std::set<int> vResult;
 
   std::ostream_iterator<int> screen(std::cout, " ");
 
@@ -31,10 +31,7 @@ int main()
   std::sort(v3.begin(), v3.end());
 
   std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::inserter(vResult, vResult.begin()));
-  std::set_intersection(vResult.begin(), vResult.end(), v3.begin(), v3.end(), std::back_inserter(vResult));
-  // I'm aware this is a bad solution
-  sort(vResult.begin(), vResult.end());
-  vResult.erase(unique(vResult.begin(), vResult.end()), vResult.end()); 
+  std::set_intersection(vResult.begin(), vResult.end(), v3.begin(), v3.end(), std::inserter(vResult, vResult.begin()));
   std::cout << "\nIntersecting values:\n";
   std::copy(vResult.begin(), vResult.end(), screen);
 
